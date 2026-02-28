e886fd5387f0fca2a5f34596e0900aff524d729a
Thomas Kowalski <thom.kowa@gmail.com>
2026-02-28 12:47:00 +0100

    more optimisations

--- .github/workflows/posix-deps-apt.sh
+++ .github/workflows/posix-deps-apt.sh
@@ -3,6 +3,36 @@ apt-get update
 
 echo "Running with Thomas' changes!"
 
+START=$(date +%s)
+
+ADDITIONAL_OPTIM="true"
+if [ "$ADDITIONAL_OPTIM" = "true" ]; then
+    INITRAMFS_CONF="/etc/initramfs-tools/update-initramfs.conf"
+    DPKG_TRIGGERS="/var/lib/dpkg/triggers/File"
+
+    if [ -e $INITRAMFS_CONF ]; then
+        sudo sed -i 's/yes/no/g' $INITRAMFS_CONF
+    fi
+    echo 'set man-db/auto-update false' | sudo debconf-communicate >/dev/null
+    sudo dpkg-reconfigure man-db
+
+    if [ -e $DPKG_TRIGGERS ]; then
+        sudo sed '/fontconfig/d' -i $DPKG_TRIGGERS
+        sudo sed '/install-info/d' -i $DPKG_TRIGGERS
+        sudo sed '/mime/d' -i $DPKG_TRIGGERS
+        sudo sed '/hicolor-icon-theme/d' -i $DPKG_TRIGGERS
+    fi
+
+    echo "force-unsafe-io" | sudo tee -a /etc/dpkg/dpkg.cfg.d/force-unsafe-io
+
+    if [ -e /usr/bin/eatmydata ]; then
+        echo "eatmydata available"
+        echo -e '#!/bin/sh\nexec eatmydata /usr/bin/dpkg $@' | sudo tee /usr/local/bin/dpkg && sudo chmod +x /usr/local/bin/dpkg
+        echo -e '#!/bin/sh\nexec eatmydata /usr/bin/apt $@' | sudo tee /usr/local/bin/apt && sudo chmod +x /usr/local/bin/apt
+        echo -e '#!/bin/sh\nexec eatmydata /usr/bin/apt-get $@' | sudo tee /usr/local/bin/apt-get && sudo chmod +x /usr/local/bin/apt-get
+    fi
+fi
+
 INSTALL_RECOMMENDS=true
 
 if [ "$INSTALL_RECOMMENDS" = "true" ]; then
@@ -11,7 +41,6 @@ else
     INSTALL_RECOMMENDS_FLAG="--no-install-recommends"
 fi
 
-START=$(date +%s)
 apt-get -yq $INSTALL_RECOMMENDS_FLAG install \
     build-essential \
     pkg-config \
